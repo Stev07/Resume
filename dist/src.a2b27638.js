d71d15602089ec1f46daf4241369a906b94dc5c9
Steve <steve.dossin@gmail.com>
2019-03-28 21:10:39 +0100

    form style + color navitem

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -29766,7 +29766,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45615" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35491" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
