6a9ff70f336e7be7cb6a192847c6d8319ad02756
Steve <steve.dossin@gmail.com>
2019-10-25 16:47:56 +0200

    about responsive

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,38 +6,40 @@ class About extends React.Component {
     return (
       <div id="about">
         <section className="about">
-          <div className="about-img" data-aos="fade-right" />
-          <div className="about-me" data-aos="fade-left" >
-            <h4>About me</h4>
-            <br />
-            <p>
-              IT passionate who loves sharing his knowledge, I became a
-              professional web developer with the Becode training. Empath by
-              nature, my listening and communication skills are always
-              appreciated by teams and clients.
-            </p>
-          </div>
-          <div className="about-details" data-aos="fade-left">
-            <h4>Contact details</h4>
-            <br />
-            <p>Steve Dossin</p>
-            <p>Belgium, Liège</p>
-            <p>steve.dossin@gmail.com</p>
-            <p>https://www.stevedossin.me</p>
+          <div className="about-img" />
+          <div className="about-me">
+            <div className="about-me-about">
+              <h4>About me</h4>
+              <br />
+              <p>
+                IT passionate who loves sharing his knowledge, I became a
+                professional web developer with the Becode training. Empath by
+                nature, my listening and communication skills are always
+                appreciated by teams and clients.
+              </p>
+            </div>
+            <div className="about-me-details">
+              <h4>Contact details</h4>
+              <br />
+              <p>Steve Dossin</p>
+              <p>Belgium, Liège</p>
+              <p>steve.dossin@gmail.com</p>
+              <p>https://www.stevedossin.me</p>
+            </div>
           </div>
         </section>
 
         <section id="education" className="education">
-          <div className="education-title" data-aos="fade-right">
+          <div className="education-title">
             <p>EDUCATION</p>
           </div>
-          <div className="education-item1" data-aos="fade-right">
+          <div className="education-item1">
             <h3>Certificate relating to basic management knowledge</h3>
             <h5>
               <em>Saint-Lambert Herstal | 2013</em>
             </h5>
           </div>
-          <div className="education-item2" data-aos="fade-right">
+          <div className="education-item2">
             <h3>Accounting technician</h3>
             <h5>
               <em>Saint-Lambert Herstal | 2013</em>
