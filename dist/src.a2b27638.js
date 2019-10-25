28877b360e8e04d5b4d3007ddb9c0357ab7c0381
Steve <steve.dossin@gmail.com>
2019-10-25 16:50:50 +0200

    remove focus outline

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -35812,7 +35812,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44459" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35065" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
