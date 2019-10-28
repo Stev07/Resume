7e09748545b9e71789e5259e8aba7b4d3a966ea2
Steve <steve.dossin@gmail.com>
2019-10-29 00:21:15 +0100

    gsap hello + about

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,6 +2,37 @@ import React from "react";
 import "../scss/about.scss";
 
 class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.controller = new ScrollMagic.Controller();
+    this.animAbout = new TimelineLite();
+
+  }
+
+  componentDidMount() {
+    this.animAbout
+      .from('h4', 1, {x:'+100vw'},0)
+      .from('.about-me-about p', 1, {x:'+100vw'}, 0.3)
+      .from('.about-me-details p', 1, {x:'+100vw'}, 0.3)
+
+ 
+
+    new ScrollMagic.Scene({
+      triggerElement: "#about",
+      triggerHook: "onLeave",
+      ease: Back.easeOut.config(3),
+      // duration: 300, // scroll distance
+      offset: -300 // start this scene after scrolling for 50px
+    })
+      .setTween(this.animAbout)
+      .addTo(this.controller) // assign the scene to the controller
+      .addIndicators()
+      // .setPin(".about-me-about") // pins the element for the the scene's duration
+
+
+  }
+
+
   render() {
     return (
       <div id="about">
@@ -9,7 +40,7 @@ class About extends React.Component {
           <div className="about-img" />
           <div className="about-me">
             <div className="about-me-about">
-              <h4>About me</h4>
+              <h4 id="aboutMe">About me</h4>
               <br />
               <p>
                 IT passionate who loves sharing his knowledge, I became a
